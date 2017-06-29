98a863ea22c31de47a7a755f9bef708f53e5765b
Александр Сергеевич Сушко <sushko@skbkontur.ru>
2017-06-29 17:22:48 +0500

    Изменит разбивку на типы

--- src/Domain/Settings.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import type { UserContact } from './UserContact';
-
-export type SettingsType = {
-    login: string;
-    contacts: UserContact[];
-    subscriptions: []; // ??? пока не понятно, что за массив
-};
--- src/Domain/UserContact.js
+++
@@ -1,7 +0,0 @@
-// @flow
-export type UserContact = {
-    id: string;
-    type: string;
-    user: string;
-    value: string;
-};
