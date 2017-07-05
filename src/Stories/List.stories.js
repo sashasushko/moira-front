b2165879ca352ccb6081efd8041107510cfbab86
Александр Сергеевич Сушко <sushko@skbkontur.ru>
2017-07-05 17:11:11 +0500

    Удалит тестовый компонент List

--- src/Stories/List.stories.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-
-storiesOf('List', module).add('default', () => <div>List</div>);
