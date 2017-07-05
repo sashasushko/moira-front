aa97333a37fbfc65833a562b6c24c38273251ea6
Александр Сергеевич Сушко <sushko@skbkontur.ru>
2017-07-05 15:26:46 +0500

    Добавит компонент TriggerStateTotal

---
+++ src/Stories/TriggerStateTotal.stories.js
@@ -0,0 +1,7 @@
+// @flow
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import TriggerStateTotal from '../Components/TriggerStateTotal/TriggerStateTotal';
+
+storiesOf('TriggerStateTotal', module).add('default', () => <TriggerStateTotal />);
