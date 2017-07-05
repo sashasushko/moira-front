0ac35ba44384bade86f66db86ce9043f18ade5e4
Александр Сергеевич Сушко <sushko@skbkontur.ru>
2017-07-05 13:56:52 +0500

    Добавит компонент Tag

---
+++ src/Components/Tag/Tag.js
@@ -0,0 +1,10 @@
+// @flow
+import React from 'react';
+
+type Props = {
+    title: string;
+};
+
+export default function Tag(props: Props): React.Element<*> {
+    return <div>Tag</div>;
+}
---
+++ src/Stories/Tag.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import Tag from '../Components/Tag/Tag';
+
+storiesOf('Tag', module).add('default', () => <Tag />);
