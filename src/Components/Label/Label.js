657611bd085da7f850d06c9a84ce7c1a2a8fbae8
Александр Сергеевич Сушко <sushko@skbkontur.ru>
2017-07-05 14:33:33 +0500

    Добавит в компонент Label вариант внешнего вида

--- src/Components/Label/Label.js
+++ src/Components/Label/Label.js
@@ -1,9 +1,10 @@
 // @flow
 import React from 'react';
 
-type Props = {
+type Props = {|
     title: string;
-};
+    type: 'ok' | 'nodata' | 'warning' | 'error';
+|};
 
 export default function Label(props: Props): React.Element<*> {
     return <div>Label</div>;
