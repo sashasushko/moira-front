2c07723befec38cc0232060e707e34917c9a236b
Александр Сергеевич Сушко <sushko@skbkontur.ru>
2017-07-05 17:11:30 +0500

    Добавит компонент Table

---
+++ src/Components/Table/Table.js
@@ -0,0 +1,16 @@
+// @flow
+import React from 'react';
+
+type Props = {|
+    items: Array;
+|};
+type State = {||};
+
+export default class Table extends React.Component {
+    props: Props;
+    state: State;
+
+    render(): React.Element<*> {
+        return <div>Table</div>;
+    }
+}
---
+++ src/Stories/Table.stories.js
@@ -0,0 +1,7 @@
+// @flow
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import Table from '../Components/Table/Table';
+
+storiesOf('Table', module).add('default', () => <Table />);
