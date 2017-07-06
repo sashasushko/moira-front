29ade3130ce2dbe2b21338982b629e207ac65f1c
Александр Сергеевич Сушко <sushko@skbkontur.ru>
2017-07-06 18:40:51 +0500

    Отключит удалённые компоненты

--- src/App.js
+++ src/App.js
@@ -3,17 +3,11 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import type { IMoiraApi } from './Api/MoiraAPI';
 import MainLayout from './Layout/MainLayout';
-import Triggers from './Containers/TriggersContainer';
-import Events from './Containers/EventsContainer';
-import Trigger from './Containers/TriggerContainer';
-import Settings from './Containers/SettingsContainer';
-import Tags from './Containers/TagsContainer';
-import Notifications from './Containers/NotificationsContainer';
-import Patterns from './Containers/PatternsContainer';
+import TriggersContainer from './Containers/TriggersContainer';
 
-type Props = {
+type Props = {|
     api: IMoiraApi;
-};
+|};
 
 export default function App(props: Props): React.Element<*> {
     function RouteWithCustomProps({
@@ -32,13 +26,7 @@ export default function App(props: Props): React.Element<*> {
     return (
         <MainLayout>
             <Switch>
-                <Route exact path='/' component={Triggers} />
-                <RouteWithCustomProps exact path='/events/:id' component={Events} api={props.api} />
-                <RouteWithCustomProps exact path='/trigger/:id?' component={Trigger} api={props.api} />
-                <RouteWithCustomProps exact path='/settings' component={Settings} api={props.api} />
-                <RouteWithCustomProps exact path='/tags' component={Tags} api={props.api} />
-                <RouteWithCustomProps exact path='/notifications' component={Notifications} api={props.api} />
-                <RouteWithCustomProps exact path='/patterns' component={Patterns} api={props.api} />
+                <RouteWithCustomProps exact path='/' component={TriggersContainer} api={props.api} />
                 <Route render={() => <p>404. Page not found</p>} />
             </Switch>
         </MainLayout>
