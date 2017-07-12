1bc2def26d0d468496f292da55563b43e7d6b559
Александр Сергеевич Сушко <sushko@skbkontur.ru>
2017-07-12 10:35:12 +0500

    Поправит заголовок истории

--- src/Stories/Tag.stories.js
+++ src/Stories/Tag.stories.js
@@ -4,4 +4,4 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import Tag from '../Components/Tag/Tag';
 
-storiesOf('Items List', module).add('default', () => <Tag title='Tag title' />);
+storiesOf('Tag', module).add('default', () => <Tag title='Tag title' />);
