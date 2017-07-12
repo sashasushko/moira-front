5109cffe9906b1a3cdc159ad303ac07cba2f0203
Александр Сергеевич Сушко <sushko@skbkontur.ru>
2017-07-12 15:06:15 +0500

    Ограничит тип состояния не просто строкой, а затипизированной строкой

--- src/Domain/Metric.js
+++ src/Domain/Metric.js
@@ -1,15 +1,15 @@
 // @flow
 
+export type MetricState = 'OK' | 'NODATA' | 'WARNING' | 'ERROR';
+
 export type Metric = {|
-    state: string;
+    state: MetricState;
     timestamp: number;
     suppressed?: boolean;
     event_timestamp?: number;
     value?: number;
     maintenance?: number;
 |};
-
-export type MetricState = 'OK' | 'NODATA' | 'WARNING' | 'ERROR';
 
 export type MetricList = {
     [metric: string]: Metric;
