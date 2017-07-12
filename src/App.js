c33cdc7d85b252127c8d3141d0e782ce52a6e6b6
Александр Сергеевич Сушко <sushko@skbkontur.ru>
2017-07-12 10:31:01 +0500

    Подключит контейнер для страницы событий

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import MainLayout from './Layout/MainLayout';
 import TriggersContainer from './Containers/TriggersContainer';
+import EventsContainer from './Containers/EventsContainer';
 
 export default function App(): React.Element<*> {
     return (
         <MainLayout>
             <Switch>
                 <Route exact path='/' component={TriggersContainer} />
+                <Route exact path='/events/:id' component={EventsContainer} />
                 <Route render={() => <p>404. Page not found</p>} />
             </Switch>
         </MainLayout>
