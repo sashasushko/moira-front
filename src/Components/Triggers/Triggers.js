08f1b1787f6fd7b7950c4b3eab4569e06c3cf0d5
Александр Сергеевич Сушко <sushko@skbkontur.ru>
2017-07-24 18:54:36 +0500

    Добавит стили для Списка Триггеров

--- src/Components/Triggers/Triggers.js
+++ src/Components/Triggers/Triggers.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import type { Trigger } from '../../Domain/Trigger';
 import TriggersItem from '../TriggersItem/TriggersItem';
+import styles from './Triggers.less';
 
 type Props = {|
     items: Array<Trigger>;
@@ -10,7 +11,7 @@ type Props = {|
 export default function TriggerList(props: Props): React.Element<*> {
     const { items } = props;
     return (
-        <div>
+        <div className={styles.triggers}>
             {items.length === 0 && 'No results'}
             {items.length !== 0 && items.map(item => <TriggersItem key={item.id} data={item} />)}
         </div>
