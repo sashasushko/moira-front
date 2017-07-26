35539197d6e53aaafc1e983d12da918b82c0f790
Александр Сергеевич Сушко <sushko@skbkontur.ru>
2017-07-26 15:05:50 +0500

    Добавит обработку изменений УРЛа

--- src/Containers/TriggersContainer.js
+++ src/Containers/TriggersContainer.js
@@ -1,11 +1,13 @@
 // @flow
 import React from 'react';
-import Paging from 'retail-ui/components/Paging';
 import type { ContextRouter } from 'react-router-dom';
 import type { IMoiraApi } from '../Api/MoiraAPI';
-import { withMoiraApi } from '../Api/MoiraApiInjection';
 import type { Trigger } from '../Domain/Trigger';
-import parsePathSearch from '../Helpers/parsePathSearch';
+import { withMoiraApi } from '../Api/MoiraApiInjection';
+import queryString from 'query-string';
+import { concat, difference } from 'lodash';
+import Paging from 'retail-ui/components/Paging';
+import Toggle from 'retail-ui/components/Toggle';
 import Triggers from '../Components/Triggers/Triggers';
 import TagSelector from '../Components/TagSelector/TagSelector';
 
@@ -14,7 +16,9 @@ type State = {|
     loading: boolean;
     triggers: ?Array<Trigger>;
     tags: ?Array<string>;
+    pages: number;
 |};
+type ParsedSearch = { [key: string]: string | Array<string> };
 
 class TriggersContainer extends React.Component {
     props: Props;
@@ -22,6 +26,7 @@ class TriggersContainer extends React.Component {
         loading: true,
         triggers: null,
         tags: null,
+        pages: 1,
     };
 
     componentDidMount() {
@@ -30,55 +35,73 @@ class TriggersContainer extends React.Component {
 
     async getData(): Promise<void> {
         const { location, moiraApi } = this.props;
-        const parsedPath = parsePathSearch(location.search);
-        const page = typeof parsedPath.page === 'number' ? parsedPath.page : 0;
+        const parsedPath = this.handleParseSearch(location.search);
+        const page = Number(parsedPath.page) || 0;
         const triggerList = await moiraApi.getTriggerList(page);
         const tagList = await moiraApi.getTagList();
-        this.setState({ loading: false, triggers: triggerList.list, tags: tagList.list });
+        const pages = Math.ceil(triggerList.total / triggerList.size);
+        this.setState({ loading: false, triggers: triggerList.list, tags: tagList.list, pages });
     }
 
-    handleSelectTag(tag: string) {
-        const { search } = this.props.location;
-        const { tags: parsedTags } = parsePathSearch(search);
-        // выделить в отдельный компонент
-        const tags = typeof parsedTags === 'string' ? parsedTags.split(',') : [];
-        const separator = tags.length !== 0 ? ',' : '';
-        const tagsUrl = '?tags=' + tags.join() + separator + tag;
-        this.props.history.push(tagsUrl);
+    handleParseSearch(search: string): ParsedSearch {
+        return queryString.parse(search, { arrayFormat: 'index' });
     }
 
-    handleRemoveTag(tag: string) {
-        const { search } = this.props.location;
-        const { tags: parsedTags } = parsePathSearch(search);
-        const tags = typeof parsedTags === 'string' ? parsedTags.split(',') : [];
-        const filtredTags = tags.filter(item => item !== tag);
-        const tagsUrl = filtredTags.length !== 0 ? '?tags=' + filtredTags.join() : '/';
-        this.props.history.push(tagsUrl);
+    handleBuildSearch(search: ParsedSearch): string {
+        return '?' + queryString.stringify(search, { arrayFormat: 'index', encode: true });
+    }
+
+    handleChangeSearch(update: ParsedSearch) {
+        const { location, history } = this.props;
+        const search = {
+            ...this.handleParseSearch(location.search),
+            ...update,
+        };
+        history.push(this.handleBuildSearch(search));
     }
 
     render(): React.Element<*> {
-        const { loading, triggers, tags } = this.state;
-        const { search } = this.props.location;
-        const { tags: parsedTags } = parsePathSearch(search);
-        const selectedTags = typeof parsedTags === 'string' ? parsedTags.split(',') : [];
+        const { loading, triggers, tags, pages } = this.state;
+        const { location } = this.props;
+        const { page, notOkMetrics, tags: selectedTags } = this.handleParseSearch(location.search);
+
         return (
             <div>
+                {/* ToDo: добавить проверку на то, что берется из УРЛа */}
                 {loading && <p>Loading...</p>}
-                {tags &&
-                    <TagSelector
-                        tags={tags}
-                        selectedTags={selectedTags}
-                        onSelect={tag => this.handleSelectTag(tag)}
-                        onRemove={tag => this.handleRemoveTag(tag)}
-                    />}
-                {triggers && <Triggers items={triggers} />}
-                <Paging
-                    activePage={1}
-                    onPageChange={() => {
-                        console.log('page change');
-                    }}
-                    pagesCount={5}
-                />
+                {!loading &&
+                    <div>
+                        <div
+                            style={{
+                                display: 'flex',
+                                marginBottom: '20px',
+                                paddingBottom: '20px',
+                                borderBottom: '1px solid #ccc',
+                            }}>
+                            {tags &&
+                                <TagSelector
+                                    tags={tags}
+                                    selectedTags={Array.isArray(selectedTags) ? selectedTags : []}
+                                    onSelect={tag => this.handleChangeSearch({ tags: concat(selectedTags, tag) })}
+                                    onRemove={tag => this.handleChangeSearch({ tags: difference(selectedTags, [tag]) })}
+                                />}
+                            <div style={{ marginLeft: 'auto', width: '140px', flexShrink: 0 }}>
+                                <Toggle
+                                    checked={notOkMetrics === 'true'}
+                                    onChange={checked => this.handleChangeSearch({ notOkMetrics: checked })}
+                                />{' '}
+                                Only problems
+                            </div>
+                        </div>
+                        {triggers && <Triggers items={triggers} />}
+                        <div style={{ marginTop: '30px' }}>
+                            <Paging
+                                activePage={Number(page) || 1}
+                                onPageChange={page => this.handleChangeSearch({ page })}
+                                pagesCount={pages}
+                            />
+                        </div>
+                    </div>}
             </div>
         );
     }
