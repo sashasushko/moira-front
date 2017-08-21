d9c521e71320fc585a810aed46fea6a408978b85
sashasushko <sushko@outlook.com>
2017-08-21 15:13:18 +0500

    Поправит пути к АПИ после тестов

--- src/Api/MoiraAPI.js
+++ src/Api/MoiraAPI.js
@@ -16,58 +16,60 @@ export interface IMoiraApi {
     getTriggerEvents(id: string): Promise<EventList>;
 }
 
+const server = 'http://vm-moira-all1:4445/';
+
 export default class Api implements IMoiraApi {
     getPatternList(): Promise<PatternList> {
-        const url = '/api/pattern';
+        const url = server + 'api/pattern';
         return fetch(url, {
             method: 'GET',
         }).then(response => response.json());
     }
 
     getTagList(): Promise<TagList> {
-        const url = '/api/tag';
+        const url = server + 'api/tag';
         return fetch(url, {
             method: 'GET',
         }).then(response => response.json());
     }
 
     getTagStats(): Promise<TagStatList> {
-        const url = '/api/stats';
+        const url = server + 'api/stats';
         return fetch(url, {
             method: 'GET',
         }).then(response => response.json());
     }
 
     getSettings(): Promise<Settings> {
-        const url = '/api/settings';
+        const url = server + 'api/user/settings';
         return fetch(url, {
             method: 'GET',
         }).then(response => response.json());
     }
 
     getTriggerList(page: number): Promise<TriggerList> {
-        const url = '/api/triggers?page=' + page;
+        const url = `${server}api/trigger/page?p=${page}&size=20`;
         return fetch(url, {
             method: 'GET',
         }).then(response => response.json());
     }
 
     getTrigger(id: string): Promise<Trigger> {
-        const url = '/api/trigger?id=' + id;
+        const url = `${server}api/trigger/${id}`;
         return fetch(url, {
             method: 'GET',
         }).then(response => response.json());
     }
 
     getTriggerState(id: string): Promise<TriggerState> {
-        const url = '/api/state?id=' + id;
+        const url = `${server}api/trigger/${id}/state`;
         return fetch(url, {
             method: 'GET',
         }).then(response => response.json());
     }
 
     getTriggerEvents(id: string): Promise<EventList> {
-        const url = '/api/events?id=' + id;
+        const url = `${server}api/event/${id}?p=0&size=100`;
         return fetch(url, {
             method: 'GET',
         }).then(response => response.json());
