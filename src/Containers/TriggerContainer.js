c8897bfce3f37b59d6512a6476a085a9d019f13e
sashasushko <sushko@outlook.com>
2017-08-22 11:53:03 +0500

    Добавит стек-раскладку на страницу

--- src/Containers/TriggerContainer.js
+++ src/Containers/TriggerContainer.js
@@ -10,6 +10,7 @@ import type { EventList } from '../Domain/Event';
 import TriggerInfo from '../Components/TriggerInfo/TriggerInfo';
 import TriggerCurrentState from '../Components/TriggerCurrentState/TriggerCurrentState';
 import TriggerEvents from '../Components/TriggerEvents/TriggerEvents';
+import { Container, ColumnStack, StackItem } from '../Components/Layout/Layout';
 
 const Tab = Tabs.Tab;
 type Props = ContextRouter & { moiraApi: IMoiraApi };
@@ -49,25 +50,39 @@ class TriggerContainer extends React.Component {
 
     render(): React.Element<*> {
         const { loading, activeTab, trigger, triggerState, triggerEvents } = this.state;
+        const { total = 0 } = triggerEvents || {};
 
         return (
             <Loader className='full-height' active={loading}>
                 {!loading &&
-                    <div>
-                        {trigger && <TriggerInfo data={trigger} />}
-                        <div className='container'>
-                            <Tabs
-                                value={activeTab}
-                                onChange={(targer, activeTab) => {
-                                    this.setState({ activeTab });
-                                }}>
-                                <Tab id='current'>Current state</Tab>
-                                <Tab id='history'>Events history</Tab>
-                            </Tabs>
-                            {activeTab === 'current' && triggerState && <TriggerCurrentState data={triggerState} />}
-                            {activeTab === 'history' && triggerEvents && <TriggerEvents data={triggerEvents} />}
-                        </div>
-                    </div>}
+                    <ColumnStack gap={5} marginBottom={50}>
+                        {trigger &&
+                            <StackItem>
+                                <TriggerInfo data={trigger} />
+                            </StackItem>}
+                        <StackItem>
+                            {total !== 0
+                                ? <Container>
+                                      <Tabs
+                                          value={activeTab}
+                                          onChange={(targer, activeTab) => {
+                                              this.setState({ activeTab });
+                                          }}>
+                                          <Tab id='current'>Current state</Tab>
+                                          <Tab id='history'>Events history</Tab>
+                                      </Tabs>
+                                      {activeTab === 'current' &&
+                                          triggerState &&
+                                          <TriggerCurrentState data={triggerState} />}
+                                      {activeTab === 'history' &&
+                                          triggerEvents &&
+                                          <TriggerEvents data={triggerEvents} />}
+                                  </Container>
+                                : <Container>
+                                      {triggerState && <TriggerCurrentState data={triggerState} />}
+                                  </Container>}
+                        </StackItem>
+                    </ColumnStack>}
             </Loader>
         );
     }
