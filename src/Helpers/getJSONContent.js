14ad83b060c48cfbe2de135cfc65f4f57f39acf0
sashasushko <sushko@outlook.com>
2017-08-30 20:44:30 +0500

    Объединит вспомогательные функции в один файл

--- src/Helpers/getJSONContent.js
+++
@@ -1,4 +0,0 @@
-// @flow
-export default function getJSONContent(data: { [key: string]: any }): string {
-    return 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(data, null, 2));
-}
