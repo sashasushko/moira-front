2191f8cc656ebf801ac54d7b83ecf9b3899b56bf
sashasushko <sushko@outlook.com>
2017-08-30 16:53:37 +0500

    Поправит описания типов

--- src/Domain/Tag.js
+++ src/Domain/Tag.js
@@ -1,10 +1,6 @@
 // @flow
 import type { Subscribtion } from './Subscribtion';
 
-export type TagList = {|
-    list: Array<string>;
-|};
-
 export type TagStat = {|
     data: {|
         maintenance?: boolean | number;
@@ -12,8 +8,4 @@ export type TagStat = {|
     name: string;
     subscriptions: Array<Subscribtion>;
     triggers: Array<string>;
-|};
-
-export type TagStatList = {|
-    list: Array<TagStat>;
 |};
