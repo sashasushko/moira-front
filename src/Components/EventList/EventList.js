d66cbcc7e96433a3fca9c41f14c8900c9eb88a3c
Sasha Sushko <sushko@outlook.com>
2017-09-04 17:58:10 +0500

    Добавит шапку в таблицу

--- src/Components/EventList/EventList.js
+++ src/Components/EventList/EventList.js
@@ -12,29 +12,28 @@ type Props = {|
 
 export default function EventList(props: Props): React.Element<*> {
     return (
-        <div className={cn('list')}>
+        <section className={cn('list')}>
+            <div className={cn('row', 'header')}>
+                <div className={cn('name')}>Metric</div>
+                <div className={cn('state-change')}>State change</div>
+                <div className={cn('date')}>Event time</div>
+            </div>
             {props.items.filter(item => item.state !== item.old_state).map((data, i) => {
                 const { metric, old_state: oldState, state, timestamp } = data;
                 return (
                     <div key={i} className={cn('row')}>
                         <div className={cn('name')}>{metric}</div>
                         <div className={cn('state-change')}>
-                            <div className={cn('prev-state')}>
-                                <span className={cn('state-value')} />
-                                <StatusIndicator statuses={[oldState]} size={14} />
-                            </div>
+                            <StatusIndicator statuses={[oldState]} size={14} />
                             <div className={cn('arrow')}>
                                 <Icon name='ArrowBoldRight' />
                             </div>
-                            <div className={cn('next-state')}>
-                                <StatusIndicator statuses={[state]} size={14} />
-                                <span className={cn('state-value')} />
-                            </div>
+                            <StatusIndicator statuses={[state]} size={14} />
                         </div>
                         <div className={cn('date')}>{moment(timestamp).format('MMMM D, HH:mm:ss')}</div>
                     </div>
                 );
             })}
-        </div>
+        </section>
     );
 }
